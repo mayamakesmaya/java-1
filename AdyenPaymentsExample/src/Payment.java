5dcfd704c7e50b1e95c48ed2e98fb02879037405
Cristina Rivera Calderon <cristina@nb-cristina>
2014-04-17 16:11:06 +0200

    Some additional fields included

--- AdyenPaymentsExample/src/Payment.java
+++ AdyenPaymentsExample/src/Payment.java
@@ -5,15 +5,14 @@ import java.util.Date;
 import org.apache.axis.client.Call;
 import org.apache.axis.client.Stub;
 
+import com.adyen.services.common.Address;
 import com.adyen.services.common.Amount;
 import com.adyen.services.common.Installments;
-import com.adyen.services.payment.AnyType2AnyTypeMapEntry;
 import com.adyen.services.payment.Card;
 import com.adyen.services.payment.PaymentLocator;
 import com.adyen.services.payment.PaymentPortType;
 import com.adyen.services.payment.PaymentRequest;
 import com.adyen.services.payment.PaymentResult;
-import com.adyen.services.payment.Recurring;
 
 
 public class Payment {
@@ -23,21 +22,21 @@ public class Payment {
 			
 			PaymentPortType ws = new PaymentLocator().getPaymentHttpPort( new java.net.URL( Credentials.test_address) );
 			
-			//Basic HTTP Authentication:
+			// Basic HTTP Authentication
 			( (Stub) ws )._setProperty( Call.USERNAME_PROPERTY, Credentials.ws_user );
 			( (Stub) ws )._setProperty( Call.PASSWORD_PROPERTY, Credentials.pwd_user );
 			
 			// Payment data
 			PaymentRequest request = new PaymentRequest();
 			request.setMerchantAccount( Credentials.merchant_account );
-			request.setAmount( new Amount( "GBP", 199) );
-			request.setReference( "Payment Test 1" );
 			
-			//request.setInstallments( new Installments( (short) 2) );
-			//request.setShopperEmail( "shopperemail@server.com" );
-			//request.setShopperReference( "shopperreference" );
-			//request.setFraudOffset( -100 );
+			// Your order number
+			request.setReference( "Payment Test -"+System.currentTimeMillis() );
 			
+			Amount amount = new Amount( "GBP", 199 );
+			request.setAmount( amount );
+			
+			// Payment method details
 			Card card = new Card();
 			card.setHolderName( "John Smith" );
 			card.setCvc( "737" );
@@ -46,6 +45,18 @@ public class Payment {
 			card.setNumber( "4111111111111111" );
 			request.setCard( card );
 			
+			// Billing address
+			Address billingAddress = new Address ("Amsterdam", "NL", "100", "1088TT", "NH", "Simon Carmiggeltstraat");
+			card.setBillingAddress(billingAddress);
+			
+			// Additional fields could be handy
+			request.setInstallments( new Installments( (short) 2) );
+			request.setShopperEmail( "shopperemail@server.com" );
+			request.setShopperReference( "shopperreference" );
+			request.setFraudOffset( -100 );
+			request.setShopperIP("1.1.1.1");
+			
+			// Send the request
 			PaymentResult result = ws.authorise( request );
 			
 			System.out.println( new Date() );
